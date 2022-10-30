82613c8223aca76207bca3e04368fb77f68f1b79
Kevin Xiao <xlzqwerty1@gmail.com>
2022-10-30 16:21:39 -0700

    Add load template function to script

--- scripts/Sync-ExcelBinOutput-Mod.ps1
+++ scripts/Sync-ExcelBinOutput-Mod.ps1
@@ -1,14 +1,17 @@
 # This script creates a mod.rs file containing all the generated serde_json structs for ExcelBinOutput JSON files
-$files = [System.Collections.ArrayList]@(
-    "#![cfg_attr(rustfmt, rustfmt_skip)]",
-    "#![allow(non_snake_case)]",
-    "#![allow(non_camel_case_types)]"
-)
+$files = [System.Collections.ArrayList]::new()
 
 Get-ChildItem -Path .\src\excelbinoutput\ -Exclude "mod.rs" |
 ForEach-Object {
     $file = [System.IO.Path]::GetFileNameWithoutExtension($_)
-    [void]$files.Add("pub mod $file;")
+    [void]$files.Add($file)
 }
 
-$files -join "`n" | Out-File .\src\excelbinoutput\mod.rs
+$mod_entry = { "pub mod $($_);" }
+
+@"
+#![cfg_attr(rustfmt, rustfmt_skip)]
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+$(($files | ForEach-Object $mod_entry) -join "`n")
+"@ | Out-File .\src\excelbinoutput\mod.rs
