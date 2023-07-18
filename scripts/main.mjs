16bb9012d6b89b385780474ffe49ce667cc0626e
Kevin Xiao <xlzqwerty1@gmail.com>
2023-07-18 15:47:21 -0700

    Automatically sync game data version to README (#2)

--- scripts/main.mjs
+++ scripts/main.mjs
@@ -1,9 +1,11 @@
 import { generateExcelBinOutput } from "./generate/excelbinoutput.mjs";
 import { generateLanguageEnum } from "./generate/languages.mjs";
+import { updateReadmeVersion } from "./version.mjs";
 
 async function main() {
   await Promise.all([generateExcelBinOutput(), generateLanguageEnum()]);
   console.log("Done!");
+  await updateReadmeVersion();
   // TODO: generate BinOutput Avatar/ConfigAvatar_*.json structs
 }
 
